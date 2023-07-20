70741367220176953ab8f3cb3eab098079e650f3
Rohit <kumarohit1202@gmail.com>
2023-07-20 09:43:41 +0530

    pushed js file

---
+++ p3.js
@@ -0,0 +1,21 @@
+//let a = 10
+// const b = {
+//     name: "rohit",
+//     age: 20
+// }
+//b = "rohit";
+//console.log(b);
+
+//for(i in b) console.log(i+":",b[i])
+
+//let users = ["rohit", "mohan", "sohan"]
+//for(i of users) console.log(i)
+
+// const c=()=>{
+//     console.log("bi")
+// }
+// c()
+
+//  let a = "Rohit"
+//  for(i of a)console.log(i)
+
